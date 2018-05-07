2379539d7da48107b32fa3be1451904f4dd3ecb2
va04939 <AdakiVinay@johndeere.com>
2018-05-07 23:06:50 +0530

    Merge remote-tracking branch 'origin/master' into using-Redux-store

--- src/app.js
+++ src/app.js
@@ -8,13 +8,13 @@ import PostForm from './components/post-form';
 export const App = () => {
     return (
         <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
-          </header>
-          <PostForm />
-          <hr />
-          <Posts />
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <h1 className="App-title">Welcome to React</h1>
+            </header>
+            <PostForm />
+            <hr />
+            <Posts />
         </div>
     );
 }
